03a21e0b6aaee0afa8fc81f1b1f89085ac9b82df
Charlie Cheever <ccheever@gmail.com>
2013-12-26 04:28:02 -0500

    Forgot to rebuild :/

--- index.js
+++ index.js
@@ -48,6 +48,9 @@
 
   saveTo = function(name, this_) {
     "Returns a function that takes an async function and saves what\nit passes along into global[name]";
+    if (name == null) {
+      name = "___";
+    }
     return function() {
       var args, fn;
       fn = arguments[0], args = 2 <= arguments.length ? __slice.call(arguments, 1) : [];
